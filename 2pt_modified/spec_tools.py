fixed major bug in cosmosis covariance calculation

2pt_modified/spec_tools.py
@@ -411,8 +411,18 @@ class ClCov( object ):
                                 #Get the indices in cl_var_binned these correspond to:
                                 ell_vals_bin_inds = ell_vals_bin - ell_lims[0]
                                 cl_var_unbinned_bin = cl_var_unbinned[ell_vals_bin_inds]
-                                cl_var_binned[ell_bin] = (np.sum((2*ell_vals_bin+1) * cl_var_unbinned_bin)
-                                    / np.sum(2*ell_vals_bin+1))
+                                #cl_var_binned[ell_bin] = (np.sum((2*ell_vals_bin+1) * cl_var_unbinned_bin)
+                                #    / np.sum(2*ell_vals_bin+1))
+                                #lukas edit
+                                # print("cosmosis")
+                                # print(cl_var_binned[ell_bin])
+                                # print(np.sum((2*ell_vals_bin+1)**2 *cl_var_unbinned_bin) / np.sum(2*ell_vals_bin+1)**2)
+                                # print("my math approximately same as comment:")
+                                # print(np.mean(cl_var_unbinned_bin)/(ell_high-ell_low))
+                                # print("or in the same way:")
+                                # print(np.sum(cl_var_unbinned_bin)/(ell_high-ell_low)**2)
+                                #major error correction in cosmosis
+                                cl_var_binned[ell_bin] = np.sum((2*ell_vals_bin+1)**2 *cl_var_unbinned_bin) / np.sum(2*ell_vals_bin+1)**2
                             cov_blocks[i_bp, j_bp] = cl_var_binned
 
                         #Now work out where this goes in the full covariance matrix