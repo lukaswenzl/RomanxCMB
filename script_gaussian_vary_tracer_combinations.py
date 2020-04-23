63ded968dd17f75a2bd89946d180b5492a65008e
lukaswenzl <ljw232@cornell.edu>
2020-04-23 00:13:12 -0400

    fixed typo

--- script_gaussian_vary_tracer_combinations.py
+++ script_gaussian_vary_tracer_combinations.py
@@ -12,16 +12,16 @@ if not os.path.exists(output_folder):
 
 
 #6x2pt
-os.system("mpirun -n "+n_processors+" cosmosis modules/WFIRSTxCMB/params_gaussian_covariance.ini -p output.filename='"+output_folder+"6x2pt.txt'")
+os.system("mpirun -n "+n_processors+" cosmosis --mpi modules/WFIRSTxCMB/params_gaussian_covariance.ini -p output.filename='"+output_folder+"6x2pt.txt'")
 
 #3x2pt
-os.system("mpirun -n "+n_processors+" cosmosis modules/WFIRSTxCMB/params_gaussian_covariance_only3x2pt.ini -p output.filename='"+output_folder+"3x2pt.txt'")
+os.system("mpirun -n "+n_processors+" cosmosis --mpi modules/WFIRSTxCMB/params_gaussian_covariance_only3x2pt.ini -p output.filename='"+output_folder+"3x2pt.txt'")
 
 #shear only
-os.system("mpirun -n "+n_processors+" cosmosis modules/WFIRSTxCMB/params_gaussian_covariance_onlyShear.ini -p output.filename='"+output_folder+"shear'.txt")
+os.system("mpirun -n "+n_processors+" cosmosis --mpi modules/WFIRSTxCMB/params_gaussian_covariance_onlyShear.ini -p output.filename='"+output_folder+"shear'.txt")
 
 #galaxy only
-os.system("mpirun -n "+n_processors+" cosmosis modules/WFIRSTxCMB/params_gaussian_covariance_onlyClustering.ini -p output.filename='"+output_folder+"galaxy'.txt")
+os.system("mpirun -n "+n_processors+" cosmosis --mpi modules/WFIRSTxCMB/params_gaussian_covariance_onlyClustering.ini -p output.filename='"+output_folder+"galaxy'.txt")
 
 #cmb only
-os.system("mpirun -n "+n_processors+" cosmosis modules/WFIRSTxCMB/params_gaussian_covariance_onlyCMBlensing.ini -p output.filename='"+output_folder+"cmblensing'.txt")
+os.system("mpirun -n "+n_processors+" cosmosis --mpi modules/WFIRSTxCMB/params_gaussian_covariance_onlyCMBlensing.ini -p output.filename='"+output_folder+"cmblensing'.txt")
