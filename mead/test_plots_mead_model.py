75c0d2049f8ec4067a2fcd253e1bd25d85bc43a8
Lukas Wenzl <ljw232@cornell.edu>
2021-04-07 17:18:04 -0400

    Added a plot to test piping of growth into mead plot shows the effect on power spectrum when using modified growth in eq 21 and 22 of mead 2020 instead of limiting to wCDM

--- mead/test_plots_mead_model.py
+++ mead/test_plots_mead_model.py
@@ -14,6 +14,10 @@ output_folder = "modules/RomanxCMB/mead/plots/"
 whattodo = "power_input_zdep,neutrino_test,halofit_comparison,wdependence "
 #whattodo = ",halofit_comparison "
 #whattodo = "wdependence"
+whattodo = "power_input_zdep,growth_test"
+whattodo = "power_input_zdep,growth_gamma"
+whattodo = "mead_with_modified_growth"
+
 
 
 
@@ -54,7 +58,7 @@ if ("power_input_zdep" in whattodo):
     index = params_names.index("cosmological_parameters--gamma0")
     vector[index] = 0.6#0.545
     index = params_names.index("cosmological_parameters--gammaa")
-    vector[index] = 0.3
+    vector[index] = -0.05
 
     fig_growth = plt.figure()  # create a figure object
     ax_growth = fig_growth.add_subplot(1, 1, 1)  # create an axes object in the figure
@@ -371,6 +375,348 @@ if ("halofit_comparison" in whattodo):
     plt.legend()
     plt.savefig(output_folder+"power_spectrum_halofit_comparison_RAW.pdf")
 
+
+
+
+if ("growth_test" in whattodo):
+
+    fig_growth = plt.figure()  # create a figure object
+    ax_growth = fig_growth.add_subplot(1, 1, 1)  # create an axes object in the figure
+
+    value_w = -0.9
+    plt.figure()
+
+    i="cosmosis growth module"
+    ini = Inifile("modules/RomanxCMB/mead/params.ini")
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.quiet = True
+    pipeline.debug = False
+    pipeline.timing = False
+
+    params_names = pipeline.varied_params
+    vector = pipeline.start_vector()
+    index = params_names.index("cosmological_parameters--w")
+    vector[index] = value_w
+
+    #ini.set("growth_gamma", "gamma_parametrization",  "F")
+
+    # Make the pipeline itself
+    pipeline = LikelihoodPipeline(ini)
+
+    data = pipeline.run_parameters(vector)
+    k = data['matter_power_lin', 'k_h']
+    z = data['matter_power_lin', 'z']
+    p_lin = data['matter_power_lin', 'p_k'] 
+    Pk_lin_resamplez_baseline = interp1d(z, p_lin, axis=0)
+    k = data['matter_power_nl', 'k_h']
+    z = data['matter_power_nl', 'z']
+    p_nl = data['matter_power_nl', 'p_k'] 
+    Pk_resamplez = interp1d(z, p_nl, axis=0)
+    z_sample = 0.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 1.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 4.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+
+    ax_growth.plot(data["growth_parameters", "a"], data["growth_parameters", "d_z"], label="cosmosis module")
+
+    ## my own growth module
+    i = "my growth module"
+
+    ini = Inifile("modules/RomanxCMB/mead/params_gammagrowth.ini")
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.quiet = True
+    pipeline.debug = False
+    pipeline.timing = False
+
+    params_names = pipeline.varied_params
+    vector = pipeline.start_vector()
+    index = params_names.index("cosmological_parameters--w")
+    vector[index] = value_w
+
+    ini.set("growth_gamma", "gamma_parametrization",  "F")
+
+    # Make the pipeline itself
+    pipeline = LikelihoodPipeline(ini)
+
+    data = pipeline.run_parameters(vector)
+    # k = data['matter_power_lin', 'k_h']
+    # z = data['matter_power_lin', 'z']
+    # p_lin = data['matter_power_lin', 'p_k'] 
+    # Pk_lin_resamplez_baseline = interp1d(z, p_lin, axis=0)
+    k = data['matter_power_nl', 'k_h']
+    z = data['matter_power_nl', 'z']
+    p_nl = data['matter_power_nl', 'p_k'] 
+    Pk_resamplez = interp1d(z, p_nl, axis=0)
+    z_sample = 0.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 1.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 4.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+
+    ax_growth.plot(data["growth_parameters", "a"], data["growth_parameters", "d_z"], label="my module")
+
+    ## halofit 
+    i ="halofit"
+    ini = Inifile("modules/RomanxCMB/mead/params_halofit.ini")
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.quiet = True
+    pipeline.debug = False
+    pipeline.timing = False
+
+    params_names = pipeline.varied_params
+    vector = pipeline.start_vector()
+    index = params_names.index("cosmological_parameters--w")
+    vector[index] = value_w#-0.8
+
+    #ini.set("growth_gamma", "gamma_parametrization",  "F")
+
+    # Make the pipeline itself
+    pipeline = LikelihoodPipeline(ini)
+
+    data = pipeline.run_parameters(vector)
+    # k = data['matter_power_lin', 'k_h']
+    # z = data['matter_power_lin', 'z']
+    # p_lin = data['matter_power_lin', 'p_k'] 
+    # Pk_lin_resamplez_baseline = interp1d(z, p_lin, axis=0)
+    k = data['matter_power_nl', 'k_h']
+    z = data['matter_power_nl', 'z']
+    p_nl = data['matter_power_nl', 'p_k'] 
+    Pk_resamplez = interp1d(z, p_nl, axis=0)
+    z_sample = 0.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 1.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 4.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+
+    
+
+
+
+    plt.title("growth test mead vs halofit (see large scales)")
+    plt.xscale("log")
+    plt.xlim(10.**(-3), 100.)
+    plt.xlabel("k/h")
+    plt.ylim(0.7,1.3)
+    plt.ylabel("P / P_lin")
+
+    # Save our plot.
+    plt.legend()
+    plt.savefig(output_folder+"power_spectrum_growth_test_RAW.pdf")
+    
+    ax_growth.legend()
+    fig_growth.savefig(output_folder+"growth_cosmosis_vs_mymodule.pdf")
+
+
+if ("growth_gamma" in whattodo):
+
+    fig_growth = plt.figure()  # create a figure object
+    ax_growth = fig_growth.add_subplot(1, 1, 1)  # create an axes object in the figure
+
+    value_gamma = 0.55 + 0.1
+    plt.figure()
+
+    ## my own growth module
+    i = "my growth module"
+
+    ini = Inifile("modules/RomanxCMB/mead/params_gammagrowth.ini")
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+    pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+    pipeline.quiet = True
+    pipeline.debug = False
+    pipeline.timing = False
+
+    params_names = pipeline.varied_params
+    vector = pipeline.start_vector()
+    index = params_names.index("cosmological_parameters--gamma0")
+    vector[index] = value_gamma
+    index = params_names.index("cosmological_parameters--omnuh2")
+    vector[index] = 0.00005
+
+    ini.set("growth_gamma", "gamma_parametrization",  "T")
+
+    # Make the pipeline itself
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+    pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+
+    data = pipeline.run_parameters(vector)
+    k = data['matter_power_lin', 'k_h']
+    z = data['matter_power_lin', 'z']
+    p_lin = data['matter_power_lin', 'p_k'] 
+    Pk_lin_resamplez_baseline = interp1d(z, p_lin, axis=0)
+    k = data['matter_power_nl', 'k_h']
+    z = data['matter_power_nl', 'z']
+    p_nl = data['matter_power_nl', 'p_k'] 
+    Pk_resamplez = interp1d(z, p_nl, axis=0)
+    z_sample = 0.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 1.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 4.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+
+    ax_growth.plot(data["growth_parameters", "a"], data["growth_parameters", "d_z"], label="my module")
+
+    ## halofit 
+    i ="halofit"
+    ini = Inifile("modules/RomanxCMB/mead/params_halofit_gammagrowth.ini")
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+    pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+    pipeline.quiet = True
+    pipeline.debug = False
+    pipeline.timing = False
+
+    params_names = pipeline.varied_params
+    vector = pipeline.start_vector()
+    index = params_names.index("cosmological_parameters--gamma0")
+    vector[index] = value_gamma#-0.8
+    index = params_names.index("cosmological_parameters--omnuh2")
+    vector[index] = 0.00005
+
+    ini.set("growth_gamma", "gamma_parametrization",  "T")
+
+    # Make the pipeline itself
+    pipeline = LikelihoodPipeline(ini)
+    pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+    pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+
+    data = pipeline.run_parameters(vector)
+    k = data['matter_power_lin', 'k_h']
+    z = data['matter_power_lin', 'z']
+    p_lin = data['matter_power_lin', 'p_k'] 
+    Pk_lin_resamplez_baseline = interp1d(z, p_lin, axis=0)
+    k = data['matter_power_nl', 'k_h']
+    z = data['matter_power_nl', 'z']
+    p_nl = data['matter_power_nl', 'p_k'] 
+    Pk_resamplez = interp1d(z, p_nl, axis=0)
+    z_sample = 0.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 1.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+    z_sample = 4.
+    plt.plot(k,Pk_resamplez(z_sample) /Pk_lin_resamplez_baseline(z_sample),label=str(i)+ " with z= "+str(z_sample) )
+
+    
+
+
+
+    plt.title("growth test mead vs halofit (see large scales)")
+    plt.xscale("log")
+    plt.xlim(10.**(-3), 100.)
+    plt.xlabel("k/h")
+    plt.ylim(0.7,1.3)
+    plt.ylabel("P / P_lin")
+
+    # Save our plot.
+    plt.legend()
+    plt.savefig(output_folder+"power_spectrum_growth_gamma_RAW.pdf")
+    
+    #ax_growth.legend()
+    #fig_growth.savefig(output_folder+"growth2_cosmosis_vs_mymodule.pdf")
+
+    
+
+
+if ("mead_with_modified_growth" in whattodo):
+
+    fig_z1 = plt.figure()  # create a figure object
+    ax_z1 = fig_z1.add_subplot(1, 1, 1)  # create an axes object in the figure
+
+    value_gamma = 0.55 + 0.1
+    plt.figure()
+
+    for value_gamma in [0.45,0.475,0.50,0.525,0.55,0.575, 0.6,0.625, 0.65]:
+        ## cosmosis growth piped into mead
+
+        ini = Inifile("modules/RomanxCMB/mead/params_gammagrowth.ini")
+        ini.set("growth_gamma", "gamma_parametrization",  "T")
+        ini.set("mead", "use_cosmosis_growth",  "T")
+
+        pipeline = LikelihoodPipeline(ini)
+        pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+        pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+        pipeline.quiet = True
+        pipeline.debug = False
+        pipeline.timing = False
+
+        params_names = pipeline.varied_params
+        vector = pipeline.start_vector()
+        index = params_names.index("cosmological_parameters--gamma0")
+        vector[index] = value_gamma
+        #index = params_names.index("cosmological_parameters--omnuh2")
+        #vector[index] = 0.00005
+        data = pipeline.run_parameters(vector)
+        k = data['matter_power_nl', 'k_h']
+        z = data['matter_power_nl', 'z']
+        p_nl = data['matter_power_nl', 'p_k'] 
+        Pk_resamplez_piped = interp1d(z, p_nl, axis=0)
+
+        #internal growth from mead
+        ini = Inifile("modules/RomanxCMB/mead/params_gammagrowth.ini")
+        ini.set("growth_gamma", "gamma_parametrization",  "T")
+        ini.set("mead", "use_cosmosis_growth",  "F")
+
+        pipeline = LikelihoodPipeline(ini)
+        pipeline.set_varied("cosmological_parameters", "gamma0", 0.2, 0.9)
+        pipeline.set_varied("cosmological_parameters", "gammaa", -0.5, 0.5)
+        pipeline.quiet = True
+        pipeline.debug = False
+        pipeline.timing = False
+
+        params_names = pipeline.varied_params
+        vector = pipeline.start_vector()
+        index = params_names.index("cosmological_parameters--gamma0")
+        vector[index] = value_gamma
+        #index = params_names.index("cosmological_parameters--omnuh2")
+        #vector[index] = 0.00005
+        data = pipeline.run_parameters(vector)
+        k = data['matter_power_nl', 'k_h']
+        z = data['matter_power_nl', 'z']
+        p_nl = data['matter_power_nl', 'p_k'] 
+        Pk_resamplez_mead = interp1d(z, p_nl, axis=0)
+
+
+        z_sample = 0.
+        plt.plot(k,Pk_resamplez_piped(z_sample) /Pk_resamplez_mead(z_sample),label="$\gamma_0 = {}, z= {}$".format(value_gamma,z_sample) )
+        z_sample = 1.
+        ax_z1.plot(k,Pk_resamplez_piped(z_sample) /Pk_resamplez_mead(z_sample),"--",label="$\gamma_0 = {}, z= {}$".format(value_gamma,z_sample) )
+        # z_sample = 2.
+        # plt.plot(k,Pk_resamplez_piped(z_sample) /Pk_resamplez_mead(z_sample),":",label="$\gamma_0 = {}, z= {}$".format(value_gamma,z_sample)  )
+        # z_sample = 3.
+        # plt.plot(k,Pk_resamplez_piped(z_sample) /Pk_resamplez_mead(z_sample),":",label="$\gamma_0 = {}, z= {}$".format(value_gamma,z_sample)  )
+        # z_sample = 4.
+        # plt.plot(k,Pk_resamplez_piped(z_sample) /Pk_resamplez_mead(z_sample),":",label="$\gamma_0 = {}, z= {}$".format(value_gamma,z_sample) )
+
+
+    
+
+
+    plt.title("Adapting equations (21) and (22) for modified growth")
+    plt.xscale("log")
+    plt.xlim(10.**(-3), 100.)
+    plt.xlabel("k/h")
+    plt.ylim(0.8,1.2)
+    plt.ylabel("$P_{nl} (eq 21,22 modified) / P_{nl} (original)$")
+
+    # Save our plot.
+    plt.legend()
+    plt.savefig(output_folder+"power_spectrum_mead_with_modified_growth_RAW.pdf")
+    
+
+    ax_z1.set_title("Adapting equations (21) and (22) for modified growth")
+    ax_z1.set_xscale("log")
+    ax_z1.set_xlim(10.**(-3), 100.)
+    ax_z1.set_xlabel("k/h")
+    ax_z1.set_ylim(0.8,1.2)
+    ax_z1.set_ylabel("$P_{nl} (eq 21,22 modified) / P_{nl} (original)$")
+    ax_z1.legend()
+    fig_z1.savefig(output_folder+"power_spectrum_mead_with_modified_growth_z1_RAW.pdf")
 
     
 
