f0fc7b489e1aa0959ca45eb3bc4d6f6d8b9ae4ff
Lukas Wenzl <ljw232@cornell.edu>
2021-08-06 14:07:16 -0400

    Adding w0wa limit for importance sampling

--- miscellaneous/check_w0_wa.py
+++ miscellaneous/check_w0_wa.py
@@ -10,9 +10,11 @@ cosmo = names.cosmological_parameters
 
 def setup(options):
     w_infinite_cutoff = options.get_double(option_section, "w_infinite_cutoff", -0.33334)
+    enforce_0_cutoff_for_IS = options.get_bool(option_section, "enforce_0_cutoff_for_IS", False)
+
     #new_name = options.get_double(option_section, "new_name", "matter_power_nl")
 
-    return {"w_infinite_cutoff":w_infinite_cutoff}
+    return {"w_infinite_cutoff":w_infinite_cutoff, "enforce_0_cutoff_for_IS":enforce_0_cutoff_for_IS}
 
 
 
@@ -42,10 +44,23 @@ def execute(block, config):
     #     block[cosmo, 'w0'] = w
     #     block[cosmo, 'set_likelihood_minus_infinity'] = False 
 
-    if w + wa > -1./3:
-        print("Warning : w0+wa>-1/3")
-
     block[cosmo, 'set_likelihood_minus_infinity'] = False 
-    block[cosmo, 'w0'] = w
+    
 
+    if w + wa > config["w_infinite_cutoff"]:
+        print("Warning : w0+wa>-1/3")
+        
+        if(config["enforce_0_cutoff_for_IS"] and w+wa >= 0):
+            #carefull this only works for cosmosis samplers not for external programs like bayesfast
+            print("Enforcing w0wa<0 by setting likelihood to minus infitiy")
+            block[cosmo, 'set_likelihood_minus_infinity'] = True
+            #running with default cosmology to avoid crash, setting likelihood to -infinity at very end 
+            w = -1.
+            wa = 0.
+
+
+
+    block[cosmo, 'w0'] = w
+    block[cosmo, 'w'] = w
+    block[cosmo, 'wa'] = wa
     return 0
